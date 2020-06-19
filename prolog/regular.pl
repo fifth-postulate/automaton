d2782cf049918150caa729f4fbafe401b0348040
Daan van Berkel <daan.van.berkel@alliander.com>
2020-06-19 17:15:20 +0200

    create a finite automaton

---
+++ prolog/regular.pl
@@ -0,0 +1,8 @@
+:- use_module(library(clpfd)).
+
+in_language(Cs) :-
+    automaton(Cs, [source(a), sink(b), sink(c)], 
+    [arc(a, 0, a), arc(a, 1, b),
+    arc(b, 0, c),
+    arc(c, 0, c), arc(c, 1, b)]).
+    
