5c43ed444e73e46894d9ac7856252e08d97e2a98
Daan van Berkel <daan.van.berkel@alliander.com>
2020-06-20 07:08:36 +0200

    generalize valid_instructions

--- prolog/robot.pl
+++ prolog/robot.pl
@@ -1,14 +1,14 @@
 :- use_module(library(clpfd)).
 
-valid_instructions(Instructions) :-
+valid_instructions(Instructions, Heading, Start, [Xfinish, Yfinish]) :-
     commands(Instructions, Commands),
     automaton(Instructions, instruction(_, FuelCost), Commands,
-        [source(north), sink(north), sink(east), sink(south), sink(west)], 
+        [source(Heading), sink(north), sink(east), sink(south), sink(west)], 
         [arc(north, 0, north, [X, Y+1, Fuel-FuelCost]), arc(north, 1, east, [X, Y, Fuel-FuelCost]), arc(north, -1, west, [X, Y, Fuel-FuelCost]),
          arc(east, 0, east, [X+1, Y, Fuel-FuelCost]), arc(east, 1, south, [X, Y, Fuel-FuelCost]), arc(east, -1, north, [X, Y, Fuel-FuelCost]),
          arc(south, 0, south, [X, Y-1, Fuel-FuelCost]), arc(south, 1, west, [X, Y, Fuel-FuelCost]), arc(south, -1, east, [X, Y, Fuel-FuelCost]),
          arc(west, 0, west, [X-1, Y, Fuel-FuelCost]), arc(west, 1, north, [X, Y, Fuel-FuelCost]), arc(west, -1, south, [X, Y, Fuel-FuelCost])],
-        [X, Y, Fuel], [0, 0, 5], [2, 1, 0]).
+        [X, Y, Fuel], Start, [Xfinish, Yfinish, 0]).
 
 commands([], []).
 commands([Instruction|Instructions], [Command|Commands]) :-
@@ -19,5 +19,9 @@ command(instruction(forward, _), 0).
 command(instruction(right, _), 1).
 command(instruction(left, _), -1).
 
-some_instructions([instruction(forward, 2), instruction(right, 1), instruction(forward, 1), instruction(forward, 1)]).
+initial_condition(
+    [instruction(forward, 2), instruction(right, 1), instruction(forward, 1), instruction(forward, 1)],
+    north,
+    [0, 0, 5],
+    [2, 1]).
 
