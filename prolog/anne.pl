3f556f6c66d93a809440fd25f51afdbcc0880948
Daan van Berkel <daan.van.berkel@alliander.com>
2020-06-24 08:55:20 +0200

    demonstrated a automaton requested by Anne Ogborn

---
+++ prolog/anne.pl
@@ -0,0 +1,9 @@
+:- use_module(library(clpfd)).
+
+in_language(Word) :-
+    automaton(Word, _,
+        Word, [source(a), sink(a), sink(b)], 
+        [arc(a, 0, a, [C + 2]), arc(a, 1, b, [C - 1]),
+        arc(b, 1, b, [C - 1])], 
+        [C], [0], [-3]).
+ 
